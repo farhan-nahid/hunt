e8693a864f07ac5842365ac4297d19fec386fdb1
gesora96 <nahidahmed9933@gmail.com>
2022-01-16 23:18:45 +0600

    tesimonial added

--- assets/js/index.js
+++ assets/js/index.js
@@ -45,4 +45,37 @@ $(document).ready(function () {
     prevArrow: "<i class='fas fa-chevron-up service__up'></i>",
     nextArrow: "<i class='fas fa-chevron-down service__down'></i>",
   });
+
+  //testimonial image vertical slider
+
+  $('.testimonial__image__slider').slick({
+    arrows: true,
+    centerMode: true,
+    centerPadding: '0',
+    vertical: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    pauseOnHover: false,
+    autoplay: true,
+    prevArrow: "<i class='fas fa-chevron-up testimonial__up'></i>",
+    nextArrow: "<i class='fas fa-chevron-down testimonial__down'></i>",
+    asNavFor: '.testimonial__text__slider',
+    cssEase: 'linear',
+  });
+
+  //testimonial text vertical slider
+
+  $('.testimonial__text__slider').slick({
+    arrows: false,
+    vertical: true,
+    centerMode: true,
+    centerPadding: '0',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.testimonial__image__slider',
+    verticalSwiping: true,
+    speed: 0.5,
+    autoplay: true,
+    cssEase: 'linear',
+  });
 });
